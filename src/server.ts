9bb28b892f170f5be730b18ca1614133fdd8fc77
bsvleste <bvaleiro@gmail.com>
2022-06-22 16:32:44 -0300

    fisrt commit

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ messsage: 'Hello word' }));
+app.listen(3333, () => console.log('Servidor HTTP online /'));
