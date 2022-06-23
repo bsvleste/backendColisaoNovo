387020e28cb0412594e175993ae5f04e096ff414
Bruno <bvaleiro@gmail.com>
2022-06-22 22:36:56 -0300

    cnfigurando prettier

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => res.json({ messsage: 'Hello word' }));
-app.listen(3333, () => console.log('Servidor HTTP online /'));
+app.listen(3333, () => console.log('Servidor HTTP online❤'));
