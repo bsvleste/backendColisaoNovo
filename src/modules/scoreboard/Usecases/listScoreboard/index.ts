d5bf3bc7596785de6c7027a36a9fc463ee49b455
Bruno <bvaleiro@gmail.com>
2022-06-27 17:58:36 -0300

    arrumando scoreboard

--- src/modules/scoreboard/Usecases/listScoreboard/index.ts
+++
@@ -1,9 +0,0 @@
-import { ScoreboardRepository } from '../../repositories/ScoreboardRepository';
-import { ListScoreboardController } from './ListScoreboardController';
-import { ListScoreboardUseCase } from './ListScoreboardUseCase';
-const scoreboardRepository = ScoreboardRepository.getInstace();
-const listScoreboardUseCase = new ListScoreboardUseCase(scoreboardRepository);
-const listScoreboardController = new ListScoreboardController(
-  listScoreboardUseCase
-);
-export { listScoreboardController };
